9925c48ee89271ac976857f9edf144d9ff0aa5cd
aline leroy <aline.leroy.27@gmail.com>
2020-10-01 20:31:57 +0200

    Exercice solved

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
 
 
